498996e0275682e0293937c21af8f0b12c13a7eb
Zhouyinan <yinan.zhou@upai.com>
2023-10-11 09:06:31 +0800

    feature: typings add env, play show change long

--- typings/components.d.ts
+++ typings/components.d.ts
@@ -3,6 +3,18 @@ import '@vue/runtime-core'
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     CcButton: typeof import('../packages/cc-ui')['CcButton']
+    CcButtonGroup: typeof import('../packages/cc-ui')['CcButtonGroup']
+    CcCheckbox: typeof import('../packages/cc-ui')['CcCheckbox']
+    CcCheckboxGroup: typeof import('../packages/cc-ui')['CcButtonGroup']
+    CcCol: typeof import('../packages/cc-ui')['CcCol']
+    CcIcon: typeof import('../packages/cc-ui')['CcIcon']
+    CcInput: typeof import('../packages/cc-ui')['CcInput']
+    CcRow: typeof import('../packages/cc-ui')['CcRow']
+    CcTransfer: typeof import('../packages/cc-ui')['CcTransfer']
+  }
+
+  interface CompoentCustomProperties {
+    $message: typeof import('../packages/cc-ui')['CcMessage']
   }
 }
 
